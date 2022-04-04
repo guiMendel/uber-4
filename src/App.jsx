24bdacf06fd7ec1f62321ad8f0fc4783bc0606ef
guimendel <guimendeln@gmail.com>
2022-04-04 15:49:41 -0300

    ✨Adiciona novos botoes para o cliente

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,7 @@ import Button from './components/Button/Button.jsx'
 import InteractionControl from './components/InteractionControl/InteractionControl'
 
 // Icones
-import {
-  FaCar,
-  FaUserPlus,
-  FaPencilAlt,
-  FaCarSide,
-  FaPlus,
-} from 'react-icons/fa'
+import { FaUserPlus, FaPencilAlt, FaCarSide, FaPlus } from 'react-icons/fa'
 
 export default function App() {
   return (
@@ -22,19 +16,19 @@ export default function App() {
 
       {/* Contem os botoes de acoes do mapa */}
       <div className="map actions">
-        <Button name={'new-car'} help={'Adicionar novos carros'}>
-          <div className="new-car-icons">
-            <FaCarSide className="car-icon" />
-            <FaPlus className="plus-icon" />
-          </div>
+        <Button name={'new-street'} help={'Adicionar novas ruas'}>
+          <FaPencilAlt />
         </Button>
 
         <Button name={'new-client'} help={'Adicionar novos clientes'}>
           <FaUserPlus />
         </Button>
 
-        <Button name={'new-street'} help={'Adicionar novas ruas'}>
-          <FaPencilAlt />
+        <Button name={'new-car'} help={'Adicionar novos carros'}>
+          <div className="new-car-icons">
+            <FaCarSide className="car-icon" />
+            <FaPlus className="plus-icon" />
+          </div>
         </Button>
       </div>
     </div>
