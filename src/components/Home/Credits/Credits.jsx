ea1c1eff1bd2d9a457a592a9fe86b567738c095d
guimendel <guimendeln@gmail.com>
2023-03-22 10:39:52 -0300

    ✨Credits screen

--- src/components/Home/Credits/Credits.jsx
+++ src/components/Home/Credits/Credits.jsx
@@ -1,3 +1,27 @@
 export default function Credits() {
-  return <p>Credits screen</p>
+  return (
+    <>
+      <h2>
+        Cabber by{' '}
+        <a
+          className="header-link"
+          href="https://github.com/guiMendel"
+          target="_blank"
+        >
+          Gui Mendel
+        </a>{' '}
+      </h2>
+
+      <p className="credits-text">
+        This work was produced as an activity for a Computer Science Bachelor
+        discipline at the University of Brasilia (UnB).
+      </p>
+
+      <ul className="credits-text">
+        <b>Asset credits:</b>
+        <li>Car image: Ihor Biliavskyi</li>
+        <li>People images: ONYXprj</li>
+      </ul>
+    </>
+  )
 }
